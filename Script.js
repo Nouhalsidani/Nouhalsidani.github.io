fbf3082ae4fe638cbbd860329d26122dd55115c1
Nouhalsidani <malikscrmb2b@gmail.com>
2025-01-24 13:32:33 +0200

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,9 +1,24 @@
-const registernButton = document.getElementById("register");
-const LoginButton = document.getElementById("Login");
-const Container = document.getElementById("container");
-registernButton.addEventListener("click",()=>{
-    Container.classList.add("right-panel-active");
+document.addEventListener("DOMContentLoaded", () => {
+    const registernButton = document.getElementById("register");
+    const Loginghost= document.getElementById("Login");
+    const Container = document.getElementById("container");
+    var Passwordinput = document.getElementById("Password");
+    var Userinput = document.getElementById("Username");
+    const loginButton= document.getElementById("Loginbutton");
+
+    registernButton.addEventListener("click",()=>{
+            Container.classList.add("right-panel-active");
+    });
+    Loginghost.addEventListener("click",()=>{
+        Container.classList.remove("right-panel-active");
+    });
+    loginButton.addEventListener("click",()=>{
+        event.preventDefault();
+        if (Passwordinput.value === "Abraj@101" && Userinput.value === "abraj@maliks.com") {
+            window.open("https://sites.google.com/view/abcverdun/home", "_blank");
+        } 
+        else {
+            alert("Invalid username or password!");
+        }
+    });
 });
-LoginButton.addEventListener("click",()=>{
-    Container.classList.remove("right-panel-active");
-}); 
